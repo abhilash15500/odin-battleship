9b6d9c3ab905a5cf406fd3a05221e4109e5b3a5d
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-03-30 17:56:31 +0530

    feat - implement ship class and its test cases

---
+++ tests/ship.test.js
@@ -0,0 +1,49 @@
+import { Ship } from "../src/ship.js";
+
+
+describe('Tests for Ship class', () => {
+    it('Ship should create the class with correct length', () => {
+        const newShip = new Ship(4); 
+        expect(newShip.length).toBe(4);
+    });
+
+    it("Ship should initialize the hits with 0 value", ()=> {
+        const newShip = new Ship(3);
+        expect(newShip.hitCount).toBe(0);
+    })
+    
+    it("Ship should initialize the isSunk value to false", ()=> {
+        const newShip = new Ship(5);
+        expect(newShip.isSunk).not.toBeTruthy();
+    })
+
+});
+
+describe("Tests for hit method of ship class",()=>{
+    it("hit method should increase the hitCount by 2 when hit method is used twice",()=>{
+        const newShip = new Ship(2);
+        newShip.hit();
+        newShip.hit();
+        expect(newShip.hitCount).toBe(2);
+    })
+})
+
+
+describe("Tests for isSunk method of ship class",()=>{
+    it("Returns true if the number of hits a ship has recieved is equal to its size",()=>{
+        let newShip = new Ship(3);
+        newShip.hit();
+        newShip.hit();
+        newShip.hit();
+        expect(newShip.isSunk).toBe(true)
+    })
+
+    it("Returns false if the number of hits a ship has recieved is not equal to its size",()=>{
+        let newShip = new Ship(4);
+        newShip.hit();
+        newShip.hit();
+       
+        expect(newShip.isSunk).toBe(false)
+    })
+})
+
