ee13250858b02783c27c4ed87ab9d67d34571249
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-04-02 12:15:30 +0530

    feat - add dom.js and game.js files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import "./styles.css";
 console.log("hello odin enjoyers hehe <3");
 
+    // game.start;
+    //entry point for the app to run
+
+
+
+    
+// move this logic in game.js file dont keep anything thats below this code here in index js//--
+//----------------------------------------------------------------------------//
+
 // use this to initialize n place all ships
 
 // ship.forEach(element => {
@@ -10,3 +19,8 @@ console.log("hello odin enjoyers hehe <3");
 // placeShips(ship.length,placementType,validCoord)
 
 // });
+
+
+
+
+
